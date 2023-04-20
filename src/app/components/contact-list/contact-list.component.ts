046b3047ad23545b62d9f0f50c025867193cae98
AleksandarIlicc <aleksandari336@gmail.com>
2023-04-21 00:54:26 +0200

    Reorganize HomeComponent and ContactListComponent to make them cleaner

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,13 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { IContact } from 'src/app/interfaces/IContact';
+
 import { faPencilAlt, faEye, faTimes } from '@fortawesome/free-solid-svg-icons';
-import {
-  Firestore,
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-} from '@angular/fire/firestore';
+
+import { ContactsService } from 'src/app/services/contacts.service';
 
 @Component({
   selector: 'app-contact-list',
@@ -19,36 +16,23 @@ export class ContactListComponent implements OnInit {
   public emptyContactList = 'Your contact list is empty';
   public noUserPhoto: string =
     'https://icon-library.com/images/no-user-image-icon/no-user-image-icon-23.jpg';
+
   @Input() contactsList: IContact[] = [] as IContact[];
+
   icon = {
     faPencilAlt,
     faEye,
     faTimes,
   };
 
-  constructor(private firestore: Firestore) {}
+  constructor(private contactsService: ContactsService) {}
 
   ngOnInit() {}
 
-  async getContacts() {
-    const collectionRef = collection(this.firestore, 'contacts');
-
-    try {
-      this.loading = true;
-      const querySnapshot = await getDocs(collectionRef);
-      this.contactsList = querySnapshot.docs.map((item) => {
-        return { id: item.id, ...item.data() } as IContact;
-      });
-      this.loading = false;
-    } catch (err) {
-      this.loading = false;
-      console.log(err);
-    }
-  }
-
-  deleteContact(id: string) {
-    const documentRef = doc(this.firestore, 'contacts', id);
-    deleteDoc(documentRef);
-    this.getContacts();
+  async deleteContact(id: string) {
+    await this.contactsService.deleteContact(id);
+    this.contactsList = this.contactsList.filter(
+      (contact) => contact.id !== id
+    );
   }
 }
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IContact } from 'src/app/interfaces/IContact';
-import { Firestore, collection, getDocs } from '@angular/fire/firestore';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+
+import { IContact } from 'src/app/interfaces/IContact';
+
+import { ContactsService } from 'src/app/services/contacts.service';
 
 @Component({
   selector: 'app-home',
@@ -13,27 +14,15 @@ export class HomeComponent implements OnInit {
   public contacts: IContact[] = [] as IContact[];
   public contact: IContact = {} as IContact;
   public loading: boolean = true;
+  public errorMessage!: Error;
 
-  constructor(private firestore: Firestore, private router: Router) {}
+  constructor(
+    private contactsService: ContactsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getContacts();
-    // PROBLEM - When I click on single contact and I try to get back on Home page data from
-    // the Firestore don't load, so there are two solutions to fix this problem
-
-    // SOLUTION ONE - we subscribed to the router.events observable and filtered for NavigationEnd
-    // event. The NavigationEnd event is triggered when navigation ends and you can execute any
-    // logic related to that event inside the IF block
-
-    // this.router.events
-    //   .pipe(filter((event) => event instanceof NavigationEnd))
-    //   .subscribe(() => {
-    //     this.getContacts();
-    //   });
-
-    // SOLUTION TWO - we subscribed to the NavigationEnd event of the Router and then called
-    // the method getContacts to reload data when navigation ends. The filter operator is used to
-    // filtered out any other types of navigation events
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -43,18 +32,14 @@ export class HomeComponent implements OnInit {
   }
 
   async getContacts() {
-    const collectionRef = collection(this.firestore, 'contacts');
-
+    this.loading = true;
     try {
-      this.loading = true;
-      const querySnapshot = await getDocs(collectionRef);
-      this.contacts = querySnapshot.docs.map((item) => {
-        return { id: item.id, ...item.data() } as IContact;
-      });
+      await this.contactsService.getContacts();
+      this.contacts = this.contactsService.contacts;
       this.loading = false;
-    } catch (err) {
+    } catch (error) {
       this.loading = false;
-      console.log(err);
+      this.errorMessage = error as Error;
     }
   }
 }
